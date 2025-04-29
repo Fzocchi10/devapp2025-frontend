a24456a949eaff3ad118e4093b5f4bde59b99911
franco <fzocchi97@gmail.com>
2025-04-29 17:21:30 -0300

    Para levantar railway

--- src/components/apiClient/apiClient.tsx
+++ src/components/apiClient/apiClient.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://devapp2025-backend-franco-zocchi.up.railway.app";
 
 const apiClient = axios.create({
   baseURL: baseURL,
